4e8ba9e9accef6d05122ea46b26d63a4cb1d7797
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-04 14:56:17 +0100

    Profile migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170404105018) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "teams", force: :cascade do |t|
-    t.string   "name"
-    t.string   "location"
-    t.string   "stadium"
-    t.string   "abbreviation"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "username"
@@ -36,7 +24,15 @@ ActiveRecord::Schema.define(version: 20170404105018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
+  end
 
+  create_table "teams", force: :cascade do |t|
+    t.string   "name"
+    t.string   "location"
+    t.string   "stadium"
+    t.string   "abbreviation"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "users", force: :cascade do |t|
