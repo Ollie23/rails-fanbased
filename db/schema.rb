10d53d0fe6dd41bc1d2c9290600eec05c3166497
Diego Linares Leon <dlinares@equiposdeelevacion.com.mx>
2017-04-04 15:32:31 +0200

    model and controller of Venue

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404093256) do
+ActiveRecord::Schema.define(version: 20170404105123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,20 @@ ActiveRecord::Schema.define(version: 20170404093256) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "description"
+    t.integer  "rating"
+    t.time     "close"
+    t.integer  "screens"
+    t.boolean  "internet"
+    t.string   "food"
+    t.integer  "price_range"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
