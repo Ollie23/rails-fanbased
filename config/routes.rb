fa4de5842a60cae247e658ac5efc6f71b04a8150
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-04 11:41:08 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :profiles
+
+  resources :matches
+
+  resources :teams, only: [:show, :create, :new, :edit, :update, :destroy]
+
+  resources :leagues
+
+  resources :venues
+
+
 end
