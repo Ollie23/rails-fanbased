afbaf06bceff70f2a33dd81475dd86ca7b030133
Diego Linares Leon <dlinares@equiposdeelevacion.com.mx>
2017-04-04 11:36:08 +0200

    local tower try

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development?
+  mount Localtower::Engine, at: "localtower"
 end
+end
+
