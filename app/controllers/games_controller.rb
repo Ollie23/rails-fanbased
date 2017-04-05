074f37fc680ca0e4e8aa81e7c3291087fadaa653
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-04-05 10:20:36 +0200

    Adding Controller League and games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,33 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+
+    redirect_to root_path
+  end
+
+  def update
+    @game = Game.find(params[:id])
+    @game.update(game_params)
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game = game.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :country)
+  end
 end
