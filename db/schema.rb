73629c967bfed0f4624745f256ce8e28000dd470
Diego Linares Leon <dlinares@equiposdeelevacion.com.mx>
2017-04-11 14:47:19 +0200

    profile upload a picture avatar working also profile picture

--- db/schema.rb
+++ db/schema.rb
@@ -137,10 +137,9 @@ ActiveRecord::Schema.define(version: 20170410141600) do
     t.integer  "price_range"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.float    "latitude"
     t.float    "longitude"
-    t.string   "photo"
-
   end
 
   add_foreign_key "attendees", "events"
