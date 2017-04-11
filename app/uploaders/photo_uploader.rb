57d7abfbe92b7a0018022b78f44bb5e4b1cb609a
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-11 10:47:59 +0100

    Fixed Admin/Paginate conflict, added maps to heroku

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -52,6 +52,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
   class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   end
->>
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170410103321) do
-
+ActiveRecord::Schema.define(version: 20170410141600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,12 +137,9 @@ ActiveRecord::Schema.define(version: 20170410103321) do
     t.integer  "price_range"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-
     t.float    "latitude"
     t.float    "longitude"
-
     t.string   "photo"
-
   end
 
   add_foreign_key "attendees", "events"
