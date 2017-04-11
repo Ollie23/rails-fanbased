04c36cef5ced2f7cbb586523fea0960de587e889
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-11 09:25:50 +0100

    Photo added to team /profile/fan changed

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'cloudinary'
+gem 'carrierwave', '~> 0.11.2'
 
 # gem "open-uri"
 gem "nokogiri"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407080520) do
+ActiveRecord::Schema.define(version: 20170410103321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 20170407080520) do
     t.string   "abbreviation"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "photo"
   end
 
   create_table "users", force: :cascade do |t|
