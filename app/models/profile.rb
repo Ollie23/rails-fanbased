4d80c8be060259c9e238c3a633cb782b15e23879
Leonardo Luxburg <leo.von.luxburg@gmail.com>
2017-04-12 19:18:13 +0200

    Adding style to the pages

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :user
-    mount_uploader :photo, PhotoUploader
-  validates :username, :first_name, :last_name, presence: true
+  mount_uploader :photo, PhotoUploader
+  validates :username, :first_name, :last_name, presence: true, on: :update
 end
