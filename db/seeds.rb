3c77fc8151643a1e6ed17022a507421ce6a47c73
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-13 23:26:01 +0100

    Seed and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,19 @@
+ Venue.create(name:"Molly Malones", address:"Oudezijds Kolk 9, Amsterdam", description:"Molly Malones is located in one of the oldest warehouses in the Red Light District, bordering a small canal. Molly’s aims to provide top quality food, drink, service and entertainment to its customers.", close: "00:00", rating: 4, screens: 6, internet: true)
 
+ Venue.create(name:"O'Reilly's", address:"Paleisstraat 103, 1012 ZL Amsterdam", description:"Hospitality, delicious homemade food & a fine pint, served up in a friendly relaxed atmosphere, making o'reilly's a true home from home", close: "00:00", rating: 4, screens: 8, internet: true)
 
- Venue.create(name:"Molly Malones", address:"Oudezijds Kolk 9, Amsterdam", description:"Molly Malones is located in one of the oldest warehouses in the Red Light District, bordering a small canal. Molly’s aims to provide top quality food, drink, service and entertainment to its customers.", close: "00:00")
+ Venue.create(name:"Sport's Café", address:"Leidseplein 11, 1017 PS Amsterdam", description:"If you’re hungry for sports and, well, lots of food, then a visit to this place with the boring, yet aptly chosen name of Satellite Sports Café might just be in order. The place is decorated with flat screen TVs and not much else, which means it’s clearly meant for gents with a hankering for football and an appetite worthy of a Viking feast or Roman banquet.", close: "00:00", rating: 3, screens: 12, internet: true)
 
- Venue.create(name:"O'Reilly's Bar", address:"Paleisstraat 103, 1012 ZL Amsterdam", description:"Hospitality, delicious homemade food & a fine pint, served up in a friendly relaxed atmosphere, making o'reilly's a true home from home", close: "00:00")
+ Venue.create(name:"Old Sailor", address:"Oudezijds Achterburgwal 39-A", description:"Located in the center of Amsterdam, in the middle of the Red Light District you will find the best bar in Amsterdam the ''Old Sailor'', with the perfect blend of people from Amsterdam and tourists.Good music, great staff and every football game there is. The interior is very special and the atmosphere is always pleasant, the view should also not be missed!", close: "00:00", rating: 4, screens: 7, internet: true)
 
- Venue.create(name:"Sport's Café", address:"Leidseplein 11, 1017 PS Amsterdam", description:"If you’re hungry for sports and, well, lots of food, then a visit to this place with the boring, yet aptly chosen name of Satellite Sports Café might just be in order. The place is decorated with flat screen TVs and not much else, which means it’s clearly meant for gents with a hankering for football and an appetite worthy of a Viking feast or Roman banquet.", close: "00:00")
+ Venue.create(name:"Coco's Outback", address:"Thorbeckeplein 12, Amsterdam", description:"Popular Australian pub for frozen beers, hearty grub & big-screen sports, plus regular club nights.", close: "00:00", rating: 2, screens: 8, internet: true)
 
- Venue.create(name:"Old Sailor", address:"Oudezijds Achterburgwal 39-A, 1012 DA Amsterdam", description:"Located in the center of Amsterdam, in the middle of the Red Light District you will find the best bar in Amsterdam the ''Old Sailor'', with the perfect blend of people from Amsterdam and tourists.Good music, great staff and every football game there is. The interior is very special and the atmosphere is always pleasant, the view should also not be missed!", close: "00:00")
+ Venue.create(name:"Belushi's", address:"Warmoesstraat 129, 1012 JA Amsterdam", description:"Informal chain bar with TV sport and live entertainment, plus a burger menu and global comfort food.", close: "00:00", rating: 3, screens: 6, internet: true)
 
- Venue.create(name:"Coco's Outback", address:"Thorbeckeplein 12, Amsterdam", description:"Popular Australian pub for frozen beers, hearty grub & big-screen sports, plus regular club nights.", close: "00:00")
+ Venue.create(name:"Susie's Saloon", address:"O.Z.Voorburgwal 254, Amsterdam", description:"A well established bar in the center of Amsterdam with over 25 years of experience in good service and cold beer.", close: "00:00", rating: 3, screens: 6, internet: true)
 
- Venue.create(name:"Belushi's", address:"Warmoesstraat 129, 1012 JA Amsterdam", description:"Informal chain bar with TV sport and live entertainment, plus a burger menu and global comfort food.", close: "00:00")
+ Venue.create(name:"Player's Bar", address:"Kleine Gartmanplantsoen 25", description:"Player's Food & Drinks is a unique combination of a tasteful restaurant, modern day speakeasy lounge bar and thrilling night club on the weekends. The atmosphere and interior is that of a drawing room with a cosy fireplace, low-key lighting and a collection of antique books on the shelves. On the weekends it transforms into one of the city’s best known night venues. ", close: "03:00", rating: 2, screens: 10, internet: true)
 
- Venue.create(name:"Susie's Saloon", address:"O.Z.Voorburgwal 254, Amsterdam", description:"A well established bar in the center of Amsterdam with over 25 years of experience in good service and cold beer.", close: "00:00")
+ Venue.create(name:"Three Sisters Bar", address:"Rembrandtplein 19, 1017 CT Amsterdam", description:"Three Sisters Pub is a pleasant bar in the heart of Amsterdam on the Rembrandtplein where everybody is welcome and where everybody feels at home. We distinguish ourselves from other bars with our homey décor and atmosphere.", close: "03:00", rating: 4, screens: 27, internet: true)
 
- # Venue.create(name:"", address:"", description:"", close: "00:00")
-
- # Venue.create(name:"", address:"", description:"", close: "00:00")
-
- # Venue.create(name:"", address:"", description:"", close: "00:00")
+ Venue.create(name:"Big Shots Bar", address:"Warmoesstraat 94, 1012 JH Amsterdam", description:"Big Shot Bars are located in the hearth of the city and are always fun to visit. Enjoy the nightlife of Amsterdam, or just drop by for a nice drink or something to eat. Everything is possible at Big Shots Bar Cafes in Amsterdam.", close: "02:00", rating: 2, screens: 14, internet: true)
