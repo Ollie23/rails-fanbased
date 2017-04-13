c56ac1d4b79484847e8f5c1d706d529e02cabdf2
Ollie Warren <oliverwarren1@hotmail.com>
2017-04-13 13:46:57 +0100

    navbar-logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
 
 
-# Venue.create(name:"Molly Malones", address:"Red Light District, Amsterdam", description:"Molly Malones is located in one of the oldest warehouses in the Red Light District, bordering a small canal. Molly’s aims to provide top quality food, drink, service and entertainment to its customers.", photo: 'app/assets/images/molly.jpg')
+ Venue.create(name:"A Pub", address:"Red Light District, Amsterdam", description:"Molly Malones is located in one of the oldest warehouses in the Red Light District, bordering a small canal. Molly’s aims to provide top quality food, drink, service and entertainment to its customers.", close: "00:00")
